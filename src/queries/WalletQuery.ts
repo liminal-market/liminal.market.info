bd1e66e7b4bb0f1b2a8ec5d8f0a78f7349e85649
ingig <ingigauti@gmail.com>
2022-11-07 15:47:49 +0000

    show actionName instead of action

--- src/queries/WalletQuery.ts
+++ src/queries/WalletQuery.ts
@@ -43,7 +43,7 @@ export default class WalletQuery {
                 }
                 balance
                 diff
-                action
+                actionName
                 created
                 createdISO
             }
